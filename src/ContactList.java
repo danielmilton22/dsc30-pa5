5a2e027978b2338b25a6379fefb476da15225d0a
danielmilton22 <88345039+danielmilton22@users.noreply.github.com>
2021-09-01 00:56:06 -0700

    Add files via upload

--- src/ContactList.java
+++ src/ContactList.java
@@ -3,38 +3,52 @@ import java.util.*;
 public class ContactList {
 
     Person person;
+    ArrayList<Person> personArray;
+    int nelems;
+    HashMap<String, String>  hashStructure;
 
+    // add
     public boolean createContact(Person person) {
-        this.person = person;
-        return false;
+        if (hashStructure.get(person) != null) {
+            return false;
+        }
+        nelems++;
+        int sizeCatcher = personArray.size();
+        personArray.add(person);
+        hashStructure.put(person.name, String.valueOf(sizeCatcher));
+        return true;
     }
 
+    // contains
     public boolean lookupContact(String name) {
         return false;
     }
 
-
+    // search
     public Person getContact(String name) {
         return null;
     }
 
-
+    // sorted heap?
     public Person[] getContactByRange(String start, String end) {
         return null;
     }
 
+    // delete
     public boolean deleteContact(String name) {
         return true;
     }
 
     public int size() {
-        return 42;
+        return nelems;
     }
 
+    // sort
     public String[] fetchAllNames() {
         return null;
     }
 
+    // sort numbers and no duplicates
     public String[] fetchAllPhoneNumbers() {
         return null;
     }
