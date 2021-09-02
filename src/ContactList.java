829023853a76ae73dbb2411ba38068ad9d5d971b
danielmilton22 <88345039+danielmilton22@users.noreply.github.com>
2021-09-01 23:18:30 -0700

    Add files via upload

--- src/ContactList.java
+++ src/ContactList.java
@@ -2,29 +2,68 @@ import java.util.*;
 
 public class ContactList {
 
+    private static final int R = 128;
+    private Node root;
     int nelems;
-    TrieTree trieTree;
-    // add
+    ContactList trieTree;
+
+    // Node constructor
+    private static class Node {
+        boolean endOfWord;
+        ContactList.Node brachChildren[];
+        Person person;
+
+        private Node() {
+            endOfWord = false;
+            brachChildren = new ContactList.Node[R];
+        }
+
+    }
 
     public ContactList() {
-        this.trieTree = new TrieTree();
+        this.trieTree = new ContactList();
     }
 
     public boolean createContact(Person person) {
-        nelems++;
-        return trieTree.insert(person);
+        Node nodeHolder = root;
+        for (char c: person.name.toCharArray()) {
+            if (nodeHolder.brachChildren[(int) c] == null) {
+                nodeHolder.brachChildren[(int) c] = new Node();
+            }
+            nodeHolder = nodeHolder.brachChildren[(int) c];
+        }
+        if (nodeHolder.endOfWord == true){
+            return false;
+        } else {
+            nelems++;
+            nodeHolder.endOfWord = true;
+            nodeHolder.person = person;
+            return true;
+        }
     }
 
     // contains
     public boolean lookupContact(String name) {
-        boolean search = trieTree.search(name);
-        return search;
+        Node nodeHolder = root;
+        for (char c: name.toCharArray()) {
+            if (nodeHolder.brachChildren[(int) c] == null) {
+                return false;
+            }
+            nodeHolder = nodeHolder.brachChildren[(int) c];
+        }
+        return true;
     }
 
     // search
     public Person getContact(String name) {
-        Person getContact = trieTree.get(name);
-        return getContact;
+        Node nodeHolder = root;
+        for (char c: name.toCharArray()) {
+            if (nodeHolder.brachChildren[(int) c] == null) {
+                return nodeHolder.person;
+            }
+            nodeHolder = nodeHolder.brachChildren[(int) c];
+        }
+        return nodeHolder.person;
     }
 
     // sorted heap?
