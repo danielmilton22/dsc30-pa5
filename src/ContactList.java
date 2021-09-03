2b695e7991cdc9c4e13fbdbc62dba5beed0cdc3a
danielmilton22 <88345039+danielmilton22@users.noreply.github.com>
2021-09-02 17:33:43 -0700

    Add files via upload

--- src/ContactList.java
+++ src/ContactList.java
@@ -86,7 +86,6 @@ public class ContactList {
             }
             contactList.add(getContact(allNames[i]));
         }
-        contactList.add(getContact(allNames[allNames.length - 1]));
         Person[] pArray = contactList.toArray(new Person[contactList.size()]);
         return pArray;
     }
